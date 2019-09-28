e5741805254432a1a10da2463243531521a5b0bc
Andrei Barsukov <dron221297@gmail.com>
2019-09-28 14:55:15 +0500

    Изменен подход к выводу данных в блоке тестов. Добавлено генерирование исключений. Добавлено описание классов. Добавлено наследование. Почищен файл lab3

--- Lab3/Classes.py
+++ Lab3/Classes.py
@@ -1,8 +1,27 @@
+from datetime import date
+
 from pprint import pprint
 
+
 # region Блок определения классов
-class Owner():
-    # Класс, определяющий владельцев автомобилей
+class Service():
+    """
+    Основой класс
+    
+    """
+
+    # Абстрактные методы, которые будут переопределены в классах потомках
+    def __repr__(self):
+        pass
+    def __str__(self):
+        pass
+
+class Owner(Service):
+    """
+    Владелец автомобиля, содержит информацию о:
+    ФИО, адрессе проживания, номере телефона и электронной почты
+    
+    """
     
     def __init__(self, fullName, address, phoneNum, email):
         self.fullName = fullName
@@ -11,12 +30,16 @@ class Owner():
         self.email = email
 
     def __repr__(self):
-        return {"name": self.fullName, "age": self.address, "phoneNum": self.phoneNum, "email": self.email}
+        return {"name": self.fullName, "age": self.address, "phoneNum": self.phoneNum, "email": self.email}   
     def __str__(self):
         return "Владелец (ФИО = " + self.fullName + ", Возраст = " + self.address + ", Номер телефона = " + self.phoneNum + ", Е-mail = " + self.email + ")"
 
-class Auto():
-    # Класс, определяющий автомобили владельцев
+class Auto(Service):
+    """
+    Автомобиль владельца, содержит информацию о:
+    модели, годе выпуска, регистрационном номере, заводском номере, объеме двигателя и начальном пробеге
+    
+    """
 
     def __init__(self, model, year, regNumber, factoryNumber, engineVolume, startMileage):
         self.model = model
@@ -26,44 +49,100 @@ class Auto():
         self.engineVolume = engineVolume
         self.startMileage = startMileage
 
-class Book():
-    # Класс, определяющий книгу тех. осмотров
+    def __repr__(self):
+        return {"model": self.model, "year": self.year, "regNumber": self.regNumber, "factoryNumber": self.factoryNumber, "engineVolume": self.engineVolume, "startMileage": self.startMileage}   
+    def __str__(self):
+        return "Автомобиль (Модель = " + self.model + ", Год выпуска = " + self.year + ", Регистрационный номер = " + self.regNumber + ", Заводской номер = " + self.factoryNumber + ", Объем двигателя = " + self.engineVolume + ", Начальный пробег = " + self.startMileage + ")"
+
+class Book(Service):
+    """
+    Сервисная книжка автомобиля, содержащая информацию о:
+    владельце, автомобиле и записях о тех. осмотрах
+    
+    """
 
     def __init__(self, owner, auto, records):
-        self.owner = owner
-        self.auto = auto
-        self.records = records
+        if (isinstance(owner, Owner) and isinstance(auto, Auto) and isinstance(records, Record)):
+            self.owner = owner
+            self.auto = auto
+            self.records = records
+        else:
+            raise Exception("Не соответсвие типов передавемых данных!")
     
+    def addRecord(self):
+        pass
 
-class Record():
-    # Класс, определяющий тех. осмотры в книжке
+    def __repr__(self):
+        return {"owner": self.owner.__repr__(), "auto": self.auto.__repr__(), "records": self.records.__repr__()}   
+    def __str__(self):
+        return "Автомобиль (Владелец автомобиля = " + self.owner + ", Автомобиль = " + self.auto + ", Технические осмотры = " + self.records + ")"
+
+class Record(Service):
+    """
+    Записи о тех. осмотрах автомобиля, содержащие информацию о:
+    дате ТО, текущем пробеге автомобиля, проделанных работах и коментарии о ТО
+    
+    """
 
     def __init__(self, date, millage, works, recomendation):
         self.date = date
         self.millage = millage
-        self.works = works
+        if (isinstance(works, Work)):
+            self.works = works
+        else:
+            raise Exception("Не соответсвие типов передавемых данынх!")
         self.recomendation = recomendation
 
-class Work():
-    # Класс, определяющий работы тех. осмотров
+    def addWork(self):
+        pass
+
+    def __repr__(self):
+        return {"date": self.date, "millage": self.millage, "works": self.works.__repr__(), "recomendation": self.recomendation}   
+    def __str__(self):
+        return "Технический осмотр (Дата = " + self.date + ", Текущий пробег = " + self.millage + ", Проделанные работы = " + self.works + ", Коментарий = " + self.recomendation + ")"
+
+class Work(Service):
+    """
+    Работы проделанные на тех. осмотре, содержащие информацию о:
+    наименовании работы, используемых материалов, мастере, выполнившем работу и стоимость проделанной работы
     
-     def __init__(self, nameWork, materials, master, price):
+    """
+    
+    def __init__(self, nameWork, materials, master, price):
         self.nameWork = nameWork
         self.materials = materials
         self.master = master
         self.price = price
+
+    def __repr__(self):
+        return {"nameWork": self.nameWork, "materials": self.materials, "master": self.master, "price": self.price}   
+    def __str__(self):
+        return "Проделанная работа (Наименование работы = " + self.nameWork + ", Использованные материалы = " + self.materials + ", Проделанные работы = " + self.master + ", Стоимость = " + self.price + ")"
+
 # endregion
 
 # region Блок тестирования модуля
 def test():
-    owner = Owner("owner", "2", "3", "4")
-    auto = Auto("auto", "1", "1", "1", "1", "1")
-    work = Work("word", "4", "4", "4")
-    record = Record("record", "3", "3", "3")
+    owner = Owner("Иванов Иван Иванович", "г.Пермь, ул. Попова, 20", "88888888888", "email@email.com")
+    auto = Auto("Ford Mustang gt500", "2018", "в500ся", "500500", "1.8", "15000")
+    work = Work("Замена масла", "Масло", "Петров Петр Петрович", "150")
+    record = Record(date(2019,8,18), "17500",  work, "Машина в отличном состоянии")
     book = Book(owner, auto, record)
 
-    pprint(owner.__repr__())
+    # Иллюстрация полиморфизма с функцией
+    def showDataClass(classObj):
+        pprint(classObj.__repr__())
 
+    # Mro - позволяет получить иерархию наследования классов
+    pprint(Book.mro())
+    showDataClass(book)
+
+    """
+    pprint(owner.__repr__())
+    pprint(auto.__repr__())
+    pprint(work.__repr__())
+    pprint(record.__repr__())
+    """
 
 if __name__ == "__main__":
     test()
--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -2,87 +2,8 @@ from datetime import date
 
 from pprint import pprint
 
-import functions_book as fBook
-
 import Classes as classesOur
 
 
-# Данные для создания книги
-dataOwner = {"auto": {
-        "Модель": "Lada Vesta",
-        "Год выпуска": "2015",
-        "Регистрационный номер": "е215ва159",
-        "Заводской №": "109186",
-        "Объем двигателя, л": "1.8",
-        "Начальный пробег автоб, км": "158"
-    }, "owner": {
-    "ФИО": ["Иванов", "Иван", "Иванович"],
-    "Адрес": ["Россия", "Пермский край", "г.Пермь", "ул.Попова", 20, 15],
-    "Телефон": "89026316888",
-    "E-mail": "email@email.ru"
-    }
-}
-
-# Вызов метода по созданию сервисной книги
-book = fBook.сreate_book(**dataOwner)
-
-fBook.add_record(book,
-                 dateRecord=date(2015, 7, 14),
-                 mileage=10000,
-                 recomendation="Все впорядке")
-
-fBook.add_work(book,
-               0,
-               workTitle="Замена масла",
-               materials="Масло",
-               master="Иванов И.И.",
-               price="800")
-fBook.add_work(book,
-               0,
-               workTitle="Диагностика компьютера",
-               materials="",
-               master="Сидоров И.И.",
-               price="3000")
-fBook.add_work(book,
-               0,
-               workTitle="Замена колес",
-               materials="Колесо",
-               master="Петров А.Б.",
-               price="6500")
-fBook.add_record(book,
-                 dateRecord=date(2017, 7, 14),
-                 mileage=18500,
-                 recomendation="Новая машина")
-fBook.add_work(book,
-               1,
-               workTitle="Диагностика двигателя",
-               materials="",
-               master="Прокофьев Е.В.",
-               price="5000")
-fBook.add_work(book,
-               1,
-               workTitle="Замена двигателя",
-               materials="Двигатель",
-               master="Прокофьев Е.В.",
-               price="30000")
-
-# Вывод данных сервисной книги
-#pprint(book)
-
-book = classesOur.Owner("fff", "ee", "gf", "d")
-
-classesOur.showDataClass(book)
-
-
-"""Исправления по PEP8:
-    - Наличие лишних пробелов перед символом ":", а также их отсутсвие после,
-    - Отсутсвие пробела после символа "#" в коментариях,
-    - Слишком много пустых строк между блоками кода,
-    - Несоответсвие имен модулей, функций и переменных PEP8,
-    - Неверное оформление Import-секции (неверный порядок, нет группировок),
-    - Слишком длинные строки (более 79 символов),
-    - Лишние пробелы вокруг символа "=" в параметрах функций
-
-"""
 
 
